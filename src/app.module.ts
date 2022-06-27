00e0fffa7e46fe3e0308087dc88f92d2c6c71a7b
gui-nunes <guinunes456@hotmail.com>
2022-06-27 14:37:49 -0300

    removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,53 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TransactionsModule } from './transactions/transactions.module';
 import { WalletModule } from './wallet/wallet.module';
-import { AuthModule } from './auth/auth.module';
-
-import {
-  KeycloakConnectModule,
-  ResourceGuard,
-  RoleGuard,
-  AuthGuard,
-} from 'nest-keycloak-connect';
-import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [
-    UsersModule,
-    TransactionsModule,
-    WalletModule,
-    AuthModule,
-    KeycloakConnectModule.register({
-      authServerUrl: process.env.KEYCLOAK_URL,
-      realm: process.env.KEYCLOAK_REALM,
-      clientId: process.env.KEYCLOAK_CLIENT_ID,
-      secret: process.env.KEYCLOAK_SECRET,
-    }),
-  ],
+  imports: [UsersModule, TransactionsModule, WalletModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
-    // This adds a global level resource guard, which is permissive.
-    // Only controllers annotated with @Resource and
-    // methods with @Scopes
-    // are handled by this guard.
-    {
-      provide: APP_GUARD,
-      useClass: ResourceGuard,
-    },
-    // New in 1.1.0
-    // This adds a global level role guard, which is permissive.
-    // Used by `@Roles` decorator with the
-    // optional `@AllowAnyRole` decorator for allowing any
-    // specified role passed.
-    {
-      provide: APP_GUARD,
-      useClass: RoleGuard,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
